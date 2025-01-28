00486d494ccdccb8449dacbd94f0f87e9b1c8421
edwinquisbertm <edwinquisbert.m@gmail.com>
2025-01-28 06:57:19 -0400

    CRUD TS Avanzado

--- typeScript-avanzado/ts-app/src/app/products/product.service.ts
+++ typeScript-avanzado/ts-app/src/app/products/product.service.ts
@@ -5,3 +5,15 @@ export const products: Product[] = [];
 export const addProduct = (data: Product) => {
   products.push(data);
 }
+
+export const updateProduct = (id: string, changes: Product) =>{
+
+}
+
+export const deleteProduct = (id: string) => {
+
+}
+
+export const getProduct = (id: string) => {
+
+}
